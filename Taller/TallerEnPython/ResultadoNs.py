3a6a019dfeb193347b1c544f452c93599c09f5b4
Vale292005 <147650541+Vale292005@users.noreply.github.com>
2026-04-09 15:33:54 -0500

    Add ResultadoNs class for algorithm results

---
+++ Taller/TallerEnPython/ResultadoNs.py
@@ -0,0 +1,5 @@
+class ResultadoNs:
+    def __init__(self, algoritmo: str, archivo: str, tiempo_ns: int):
+        self.algoritmo = algoritmo
+        self.archivo = archivo
+        self.tiempo_ns = tiempo_ns
