20a5321a3f4b43d1c88ff1256ce48724fc7168cd
Vale292005 <147650541+Vale292005@users.noreply.github.com>
2026-04-09 15:33:30 -0500

    Create Resultado.py

---
+++ Taller/TallerEnPython/Resultado.py
@@ -0,0 +1,5 @@
+class Resultado:
+    def __init__(self, algoritmo: str, archivo: str, tiempo_ms: float):
+        self.algoritmo = algoritmo
+        self.archivo = archivo
+        self.tiempo_ms = tiempo_ms
