55a95699963ed4175d0607ba04be939994332120
GeroJagar <josegeronimojaramillogarzonj@gmail.com>
2026-04-13 13:01:29 -0500

    Se agrego un for para crear un promedio de ejecución del algoritmo
    Refactor binary search performance measurement and average timing calculation.

--- Taller/TallerEnJava/src/main/java/com/analisis/algoritmos/BinarySearch.java
+++ Taller/TallerEnJava/src/main/java/com/analisis/algoritmos/BinarySearch.java
@@ -33,15 +33,25 @@ public class BinarySearch {
 
         DualPivotQuicksort.dualPivotQuickSort(datos, 0, datos.length - 1);
 
+        int resultado = -1;
+
+        int repeticiones = 100;
+
+        for (int i = 0; i < 50; i++) {
+            binarySearch(datos, 0, datos.length - 1, numero);
+        }
+
         long inicio = System.nanoTime();
 
-        int resultado = binarySearch(datos, 0, datos.length - 1, numero);
+        for (int i = 0; i < repeticiones; i++) {
+            resultado = binarySearch(datos, 0, datos.length - 1, numero);
+        }
 
         long fin = System.nanoTime();
 
-        long tiempoNs = fin - inicio;
+        long tiempoPromedioNs = (fin - inicio) / repeticiones;
 
-        System.out.println("Tiempo (ns): " + tiempoNs);
+        System.out.println("Tiempo promedio (ns): " + tiempoPromedioNs);
 
         if (resultado != -1) {
             System.out.println("Número encontrado en posición: " + resultado);
@@ -49,7 +59,7 @@ public class BinarySearch {
             System.out.println("Número NO encontrado");
         }
 
-        ArchivoUtils.agregarResultadoNs(algoritmo, nombreArchivo, tiempoNs);
+        ArchivoUtils.agregarResultadoNs(algoritmo, nombreArchivo, tiempoPromedioNs);
     }
 
     public static void main(String[] args) {
@@ -65,7 +75,6 @@ public class BinarySearch {
         probarArchivo("archivo_100k.txt", numeroBuscado, algoritmo);
         probarArchivo("archivo_1M.txt", numeroBuscado, algoritmo);
 
-        // 🔥 Exportar resultados
         ArchivoUtils.exportarCSVNs(algoritmo);
         ArchivoUtils.exportarJSONNs(algoritmo);
     }
